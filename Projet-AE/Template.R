24f76eb7d969fca245401795ebbc7ae280cf3f72
ncorbiere <nicolas.corbiere@amadeus.com>
2021-12-31 10:48:47 +0100

    Template

--- Projet-AE/Template.R
+++ Projet-AE/Template.R
@@ -1,10 +1,7 @@
 d1=read.table("../BDD/student-mat.csv",sep=",",header=TRUE)
-View(d1)
 d2=read.table("../BDD/student-por.csv",sep=",",header=TRUE)
-View(d2)
 mydata=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet","guardian","traveltime","studytime","health","Walc","Dalc","goout","freetime","famrel","romantic","higher","activities","famsup","schoolsup"))
 
 View(mydata)
 
-print(mydata$schoolsup.x)
-print(mydata$schoolsup.y)
+
